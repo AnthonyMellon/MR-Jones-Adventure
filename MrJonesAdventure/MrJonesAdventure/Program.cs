bc60d95cbd2dffef628c90cba0a7a4d37eda437c
Todd Meason (1000027339) <MEAST2@student.op.ac.nz>
2019-06-05 14:08:55 +1200

    Added Introduction method

--- MrJonesAdventure/MrJonesAdventure/Program.cs
+++ MrJonesAdventure/MrJonesAdventure/Program.cs
@@ -54,7 +54,7 @@ namespace MrJonesAdventure
             }
             if (selection == 0)
             {
-                StartRoom();
+                Intro();
             }
             else if (selection == 1)
             {
@@ -131,6 +131,28 @@ namespace MrJonesAdventure
         static void Room2()
         {
 
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine("Welcome to Mr Jones Adventure");
+            Console.WriteLine("This is a text based adventure game, you will be given options on screen to choose from such as solving puzzles or avoiding traps");
+            Console.WriteLine("Your goal is to find treasure within the tomb and escape without dying");
+            Console.WriteLine();
+            Console.WriteLine("Are you ready to begin");
+            Console.WriteLine("Yes".PadRight(20) + "No");
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                StartRoom();
+            }
+            else if (Console.ReadLine().ToLower() == "no")
+            {
+                MainMenu();
+            }
+            else
+            {
+                Console.WriteLine("I think you may have trouble with this game. Please enter Yes or No");
+            }
         }
 
         static int NumLoop(int input, int min, int max)
